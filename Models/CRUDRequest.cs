0b977ce37d216f6d54d9ef57b6c1c0d7ea1ec75a
HaidarMalulein25 <78938789+HaidarMalulein25@users.noreply.github.com>
2021-02-12 01:47:40 +0200

    Add files via upload

---
+++ Models/CRUDRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserAuthentication.Models
+{
+    public class CRUDRequest
+    {
+        public string id { get; set; }
+        [Required]
+        public string accesstoken { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+    }
+}
---
+++ Models/RefreshTokenRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserAuthentication.Models
+{
+    public class RefreshTokenRequest
+    {
+        public string id { get; set; }
+        public string accesstoken { get; set; }
+        public string refreshtoken { get; set; }
+    }
+}
